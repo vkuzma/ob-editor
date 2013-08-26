2a64ffc85eacd2b7c5cdb5291024dadfc3fe0834
vkuzma <vladimirkuzma.ch@gmail.com>
2013-08-26 22:53:55 +0200

    working on ui elements

--- js/edit_guru.js
+++ js/edit_guru.js
@@ -4,14 +4,14 @@ var EditGuru = function(options) {
 	}
 	this.$container = $(this.container);
 	this.$content = this.$container.find('#content');
-	this.regBlock = '<div class="block"><textarea></textarea></div><div class="space"></div>';
+	this.regBlock = '<div class="block" style="opacity: 0;"><div class="text" contentEditable="true"></div></div><div class="space"><a href=""class="add">+</a></div>';
 	this.init();
 };
 
 EditGuru.prototype = {
 	init: function() {
 		if(this.$container) {
-			this.$content.on('click', '.space', $.proxy(function(event) {
+			this.$content.on('click', '.add', $.proxy(function(event) {
 				event.preventDefault();
 				if(event.target == event.currentTarget)
 					this.insert_block($(event.target));
@@ -21,19 +21,22 @@ EditGuru.prototype = {
 	insert_block: function(spacer) {
 		var blocks = this.$content.find('>.block');
 		var click_pos = blocks.length;
+		var new_block = $(this.regBlock);
 		var match = false;
-		blocks.each(function(index, value) {
-		console.log($(value).position().top+'  '+spacer.position().top);
+		blocks.each($.proxy(function(index, value) {
 			if($(value).position().top > spacer.position().top) {
 				click_pos = index
-				$(value).before(this.regBlock);
+				$(value).before(new_block);
 				match = true;
 				return false;
 			}
-		});
-		console.log(click_pos);
+		}, this));
 
 		if(!match)
-			this.$content.append(this.regBlock);
+			this.$content.append(new_block);
+
+		new_block.animate({
+			opacity: 1
+		}, 500)
 	}	
 }
