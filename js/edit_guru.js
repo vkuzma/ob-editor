4b0c095b6b184bd1825aac5d14b6974a614fa4bf
vkuzma <vladimirkuzma.ch@gmail.com>
2013-09-05 01:26:14 +0200

    experimenting with dom nodes

--- js/edit_guru.js
+++ js/edit_guru.js
@@ -21,13 +21,80 @@ EditGuru.prototype = {
 				// Enter
 				if(event.keyCode == 13) {
 					event.preventDefault();
-					console.log(window.getSelection());
+					var selObj = window.getSelection();
+					console.log(selObj);
+					console.log(selObj.focusNode.nodeType);
+
+					// textrange selected
+					if(selObj.type == 'Range') {
+						// select from right to left
+						if(selObj.extentOffset > selObj.baseOffset) {
+							var n1 = selObj.focusNode.splitText(selObj.extentOffset);
+							var n2 = selObj.focusNode.splitText(selObj.baseOffset);
+							var n0 = selObj.focusNode.splitText(0);
+						}
+						// select from left to right
+						else {
+							var n1 = selObj.focusNode.splitText(selObj.baseOffset);
+							var n2 = selObj.focusNode.splitText(selObj.extentOffset);
+							var n0 = selObj.focusNode.splitText(0);
+						}
+						$(n2).css('font-weight', 'bold');
+						var newElement = $('<span style="color: grey;">');
+						newElement.append(n2);
+						$(selObj.focusNode.parentElement).html(n0).append(newElement).append(n1);
+					}
+					// $(this).html([$(this).html().slice(0, selObj.extentOffset), $(this).html().slice(selObj.extentOffset)].join('<br>'));
+					var node = this;
+		   			var range = window.getSelection().getRangeAt(0);
+
+		   			var treeWalker = document.createTreeWalker(
+					        node,
+					        NodeFilter.SHOW_ELEMENT,
+					        null,
+					        false
+					    );
+					    var charCount = 0;
+					    while (treeWalker.nextNode()) {
+					        //console.log(treeWalker.currentNode);
+					    }
 				}
 			});
 			this.$container.find('.bold').click(function(event) {
 				event.preventDefault();
-				alert("bold");
+				var selObj = window.getSelection();
+				var $parentNode = $(selObj.focusNode.parentNode);
+				if(selObj.type == 'Caret') {
+					if($parentNode.css('font-weight') == 'bold')
+						$parentNode.css('font-weight', '');
+					else
+						$parentNode.css('font-weight', 'bold');
+				}
 			});
+			this.$container.find('.italic').click(function(event) {
+				event.preventDefault();
+				var selObj = window.getSelection();
+				var $parentNode = $(selObj.focusNode.parentNode);
+				if(selObj.type == 'Caret') {
+					if($parentNode.css('font-style') == 'italic')
+						$parentNode.css('font-style', '');
+					else
+						$parentNode.css('font-style', 'italic');
+				}
+			});
+			this.$container.find('.underline').click(function(event) {
+				event.preventDefault();
+				var selObj = window.getSelection();
+				var $parentNode = $(selObj.focusNode.parentNode);
+				if(selObj.type == 'Caret') {
+					if($parentNode.css('text-decoration') == 'underline')
+						$parentNode.css('text-decoration', '');
+					else
+						$parentNode.css('text-decoration', 'underline');
+				}
+			});
+
+
 		}
 	},
 	// insert a regular block (p tag)
@@ -56,7 +123,7 @@ EditGuru.prototype = {
 	exportHtmlCode: function() {
 		var exportedHTML = '';
 		this.$content.find('>.block').each(function(index, value) {
-			exportedHTML += '<p>' + $(value).find('.text').text() + '</p>\n';
+			exportedHTML += '<p>' + $(value).find('.text').html() + '</p>\n';
 		});
 		return exportedHTML;
 	},
